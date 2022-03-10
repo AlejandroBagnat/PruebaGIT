b42d518c145aaca9e9cbb1930a0939b9746b2c89
TgoMt <thiaguiduran@hotmail.com>
2022-03-10 17:49:43 -0300

    modificacion

--- date.js
+++ date.js
@@ -2,3 +2,5 @@ let fecha = new Date();
 
 console.log("Día: "+fecha.getDate());
 console.log('Hoy es el día '+ fecha.getDate() +' del mes '+ (fecha.getMonth() + 1) +' del año '+ fecha.getFullYear());
+
+console.log("Soy Thiago");
